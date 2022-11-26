06d5473859bc9450cb2fc6b4742c8c4db46aab5c
Kamboy 123 <89111294+TheKamboy@users.noreply.github.com>
2022-11-25 20:54:22 -0800

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,16 @@
+@echo off
+title 3D Pinball Installer
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+echo Installing...
+echo Select Directory:
+xcopy /E Pinball "C:\Pinball"
+set TARGET='C:\Pinball\PINBALL.EXE'
+set SHORTCUT='C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Pinball.lnk'
+set PWS=powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile
+
+%PWS% -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut(%SHORTCUT%); $S.TargetPath = %TARGET%; $S.Save()"
+
+echo Finished! You can find Pinball in your Start Menu.
+pause
+exit
